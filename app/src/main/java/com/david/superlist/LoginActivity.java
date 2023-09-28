8a179fed389aa099f28c17a0becf039c6fba1ecf
David <davidsuarezperez@alumno.ieselrincon.es>
2023-09-28 19:55:46 +0100

    Terminar de pulir el login

--- app/src/main/java/com/david/superlist/LoginActivity.java
+++ app/src/main/java/com/david/superlist/LoginActivity.java
@@ -2,26 +2,32 @@ package com.david.superlist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button botonLogin;
+//    Button botonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        botonLogin = findViewById(R.id.buttonLogIn);
+//        botonLogin = findViewById(R.id.buttonLogIn);
+//
+//        botonLogin.setOnClickListener(view -> {
+//
+//
+//
+//        });
 
-        botonLogin.setOnClickListener(view -> {
+        TextView texto = findViewById(R.id.loginForgotPassword);
 
-            System.out.println("Hola");
-
-        });
+        texto.setOnClickListener(view -> texto.setVisibility(View.INVISIBLE));
 
     }
 }
--- app/src/main/java/com/david/superlist/MainActivity.java
+++ app/src/main/java/com/david/superlist/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +15,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+        System.out.println("hola");
+        boton = findViewById(R.id.botonIr);
 
-//        boton = findViewById(R.id.botonIr);
-//
-//        boton.setOnClickListener(view -> startActivity(new Intent(this,LoginActivity.class)));
+        boton.setOnClickListener(view -> startActivity(new Intent(this,LoginActivity.class)));
+
+
 
     }
 }
