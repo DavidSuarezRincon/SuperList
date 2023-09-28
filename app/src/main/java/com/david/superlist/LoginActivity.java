80e482cba5b7f0c0a2a959753723b514d8729ea9
David <davidsuarezperez@alumno.ieselrincon.es>
2023-09-28 19:19:03 +0100

    Cambio estructura algunas cosas

--- app/src/main/java/com/david/superlist/LoginActivity.java
+++ app/src/main/java/com/david/superlist/LoginActivity.java
@@ -1,16 +1,27 @@
 package com.david.superlist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button botonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        botonLogin = findViewById(R.id.buttonLogIn);
+
+        botonLogin.setOnClickListener(view -> {
+
+            System.out.println("Hola");
+
+        });
 
     }
 }
--- app/src/main/java/com/david/superlist/MainActivity.java
+++ app/src/main/java/com/david/superlist/MainActivity.java
@@ -2,13 +2,22 @@ package com.david.superlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+//        boton = findViewById(R.id.botonIr);
+//
+//        boton.setOnClickListener(view -> startActivity(new Intent(this,LoginActivity.class)));
+
     }
 }
