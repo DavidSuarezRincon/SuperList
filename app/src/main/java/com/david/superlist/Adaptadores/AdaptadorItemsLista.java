251e2009fc07467208b848a614ff494910778817
David <davidsuarezperez@alumno.ieselrincon.es>
2023-12-11 17:10:04 +0000

    corrijo error preservación de lista

--- app/src/main/java/com/david/superlist/Adaptadores/AdaptadorItemsLista.java
+++ app/src/main/java/com/david/superlist/Adaptadores/AdaptadorItemsLista.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/david/superlist/pojos/TareaLista.java
+++ app/src/main/java/com/david/superlist/pojos/TareaLista.java
@@ -11,13 +11,11 @@ public class TareaLista implements Parcelable {
 
     private String task;
     private String priority;
-    private boolean isChecked;
     private int iconPriorityColor;
 
     public TareaLista(String task, String priority) {
         this.task = task;
         this.priority = priority;
-        isChecked = false;
     }
 
     private TareaLista(Parcel in) {
@@ -38,13 +36,6 @@ public class TareaLista implements Parcelable {
         return priority;
     }
 
-    public boolean isChecked() {
-        return isChecked;
-    }
-
-    public void setChecked(boolean checked) {
-        isChecked = checked;
-    }
 
     public void setPriority(String priority) {
         this.priority = priority;
@@ -88,11 +79,11 @@ public class TareaLista implements Parcelable {
         if (this == o) return true;
         if (!(o instanceof TareaLista)) return false;
         TareaLista that = (TareaLista) o;
-        return isChecked() == that.isChecked() && getIconPriorityColor() == that.getIconPriorityColor() && Objects.equals(getTask(), that.getTask()) && Objects.equals(getPriority(), that.getPriority());
+        return getIconPriorityColor() == that.getIconPriorityColor() && Objects.equals(getTask(), that.getTask()) && Objects.equals(getPriority(), that.getPriority());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getTask(), getPriority(), isChecked(), getIconPriorityColor());
+        return Objects.hash(getTask(), getPriority());
     }
 }
